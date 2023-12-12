f2738bc0750eb5410003614ac19a9d79365b3eb2
Matej Postolka <postolka@cegelec.cz>
2023-12-12 13:06:13 +0100

    WIP fix firmware blob lib indentation

--- lib/firmware_blob/validate_blob.h
+++ lib/firmware_blob/validate_blob.h
@@ -4,23 +4,24 @@
 #include "boot_policy.h"
 #include "firmware_blob.h"
 
-typedef enum
-{
-    BOARD_PARTITION_NONE,
-    BOARD_PARTITION_RECOVERY,
-    BOARD_PARTITION_SOFTWARE,
-    BOARD_PARTITION_COUNT,
+typedef enum {
+  BOARD_PARTITION_NONE,
+  BOARD_PARTITION_RECOVERY,
+  BOARD_PARTITION_SOFTWARE,
+  BOARD_PARTITION_COUNT,
 } board_partition_t;
 
 struct validate_blob_input
 {
-    const void *blob;
-    uint32_t blob_len;
-    board_boot_policy_t board_boot_policy;
-    uint32_t board_project_id;
-    board_partition_t board_partition;
+  const void *blob;
+  uint32_t blob_len;
+  board_boot_policy_t board_boot_policy;
+  uint32_t board_project_id;
+  board_partition_t board_partition;
 };
 
-validate_blob_err_t validate_blob(const struct validate_blob_input *validate_blob_input, const char **feedback);
+validate_blob_err_t
+validate_blob(const struct validate_blob_input *validate_blob_input,
+              const char **feedback);
 
 #endif // FIRMWARE_BLOB_VALIDATE_BLOB_H
--- lib/firmware_blob/verify_blob_integrity.h
+++ lib/firmware_blob/verify_blob_integrity.h
@@ -1,8 +1,11 @@
-#ifndef __VERIFY_BLOB_INTEGRITY_H
-#define __VERIFY_BLOB_INTEGRITY_H
+#ifndef FIRMWARE_BLOB_VERIFY_BLOB_INTEGRITY_H
+#define FIRMWARE_BLOB_VERIFY_BLOB_INTEGRITY_H
 
 #include "firmware_blob.h"
 
-validate_blob_err_t verify_blob_integrity(const void *blob, uint32_t blob_len, struct blob_header *parsed_blob_header_out, int *has_valid_signature);
+validate_blob_err_t
+verify_blob_integrity(const void *blob, uint32_t blob_len,
+                      struct blob_header *parsed_blob_header_out,
+                      int *has_valid_signature);
 
-#endif // __VERIFY_BLOB_INTEGRITY_H
+#endif // FIRMWARE_BLOB_VERIFY_BLOB_INTEGRITY_H
